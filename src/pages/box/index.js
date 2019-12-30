023bde179cab6e47f5a71dd6009eb3bf8b704616
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-30 19:27:01 -0300

    removido imports que nao estavam sendo usados

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -1,48 +1,47 @@
-import React, { Component } from "react";
-import api from "../../services/api";
-import logo from "../../assets/logo.svg";
+import React, { Component } from 'react'
+import api from '../../services/api'
+import logo from '../../assets/logo.svg'
 
-import { Link } from "react-router-dom";
-import { MdInsertDriveFile } from "react-icons/md";
-import "./styles.css";
+import { MdInsertDriveFile } from 'react-icons/md'
+import './styles.css'
 
 export default class Box extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       box: {}
-    };
+    }
   }
 
-  async componentDidMount() {
-    const box = this.props.match.params.id;
-    const response = await api.get(`boxes/${box}`);
+  async componentDidMount () {
+    const box = this.props.match.params.id
+    const response = await api.get(`boxes/${box}`)
 
     this.setState({
       box: response.data
-    });
+    })
   }
 
-  render() {
+  render () {
     return (
-      <div id="box__container">
+      <div id='box__container'>
         <header>
-          <img src={logo} alt="logo" />
+          <img src={logo} alt='logo' />
           <h1>{this.state.box.title}</h1>
         </header>
         <ul>
           {this.state.box.files &&
             this.state.box.files.map(file => (
               <li>
-                <a href={file.url} className="file__info" target="_blank">
-                  <MdInsertDriveFile size={24} color="#A5Cfff" />
+                <a href={file.url} className='file__info' target='_blank'>
+                  <MdInsertDriveFile size={24} color='#A5Cfff' />
                   <strong>{file.title}</strong>
-                </a>{" "}
+                </a>{' '}
                 <span>{file.createdAt}</span>
               </li>
             ))}
         </ul>
       </div>
-    );
+    )
   }
 }
