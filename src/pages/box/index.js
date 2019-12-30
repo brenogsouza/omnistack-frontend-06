2c8368700d589708b7bd43f192ef16d1e2dd67f2
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-30 17:22:23 -0300

    criado page box

--- src/pages/box/index.js
+++ src/pages/box/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react'
 
+import logo from '../../assets/logo.svg'
+import { MdInsertDriveFile } from 'react-icons/md'
 import './styles.css'
 
 export default class Box extends Component {
   render () {
     return (
-      <div>
-        <h1>Ola mundo</h1>
+      <div id='box__container'>
+        <header>
+          <img src={logo} alt='logo' />
+          <h1>TESTE</h1>
+        </header>
+        <ul>
+          <li>
+            <a className='file__info' href="">
+              <MdInsertDriveFile size={24} color='#A5Cfff' />
+              <strong>desafio.pdf</strong>
+            </a>
+            <span>há 3 minutos atrás</span>
+          </li>
+          <li>
+            <a className='file__info' href="">
+              <MdInsertDriveFile size={24} color='#A5Cfff' />
+              <strong>desafio.pdf</strong>
+            </a>
+            <span>há 3 minutos atrás</span>
+          </li>
+        </ul>
       </div>
     )
   }
